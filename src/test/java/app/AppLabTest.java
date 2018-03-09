ddff5a951bc6ac07fab1b6689109b82b269565dc
cmart <cmart@LenovoCM>
2018-03-09 17:03:18 -0300

    Testes e codigo

---
+++ src/test/java/app/AppLabTest.java
@@ -0,0 +1,24 @@
+package app;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppLabTest {
+    
+    public AppLabTest() {
+    }
+
+    /**
+     * Test of lerJSON method, of class AppLab.
+     */
+    @Test
+    public void testLerJSON() throws Exception {
+        System.out.println("lerJSON");
+        String url = "http://time.jsontest.com/";
+        String result = AppLab.lerJSON(url);
+        assertTrue(result.length() > 0);
+    }
+    
+}
